bd9715664324b9e7daaa335454bca9beab7b86ee
HP PRODUCE <produce@gmail.com>
2024-04-12 19:20:22 -0500

    wow

--- src/json/wow.js
+++ src/json/wow.js
@@ -1,37 +1,128 @@
-const wow = [
+const categorias = [
   {
-    brand: 'Jean Paul Creations',
-    products: [
+    id: 1,
+    nombrecategoria: 'Alimentos y Bebidas',
+    imagen: 'src/assets/img/wow/servicios.png'
+  },
+  {
+    id: 2,
+    nombrecategoria: 'Mascotas',
+    imagen: 'src/assets/img/wow/servicios.png'
+  },
+  {
+    id: 3,
+    nombrecategoria: 'Moda y Belleza',
+    imagen: 'src/assets/img/wow/servicios.png'
+  },
+  {
+    id: 4,
+    nombrecategoria: 'Deco y Hogar',
+    imagen: 'src/assets/img/wow/servicios.png'
+  },
+ {
+    id: 5,
+    nombrecategoria: 'Servicios y otros',
+    imagen: 'src/assets/img/wow/servicios.png'
+  },
+ {
+    id: 6,
+    nombrecategoria: 'Infantil',
+    imagen: 'src/assets/img/wow/servicios.png'
+  },
+];
+
+
+
+const marcas = [
+  {
+    id: 1,
+    imagen: 'https://lh3.googleusercontent.com/fife/ALs6j_HSGQec2t0izB-Ie4vRt33nGAYgFIj4G07lQ4Z7M85HXsz9EEJT6TQDGWG55ZMfDKLKbQn5fZm20q69PUmxiVNG5Fksu7oHz-VP65mGqJ-XfMKNZPWXgZh4gb6zIOFI2z_BW7PKdvFgxGCKC62vl1CKYSY5rewXYNAnLXHYbBKX7JkwWTaIC-2CDnPhhSTKzcAYhx2-Om8A69YceDAVbG4rog75tanx7OCd3CvlTT8BUDR179voTtZmZA9eidfTmr8BwG3uO4RSLH7F901OcKvpBl0qCkldUVsPxg-DzpC_aQJm-t93cSDvgL_TK6W3iKxnhNXpRuuZR-tZ255mEZx7NFX24JuV_PwZ07VE6n-UgPG_-Jx7iRjoWuy_BMnKnEK4QjOYau4o-LZ9YOsPvuSgmBcohjl9bBFWmCaXTkOLch8gmkHYfZG8N7GkuuADAeNchhoHbJ87aSO_lKfIpZyuOIYjF2rdcG2t1N2eREn2JNU2YshJzixpFUhOhZD294E27q0bSHFyB8H8xV8yV1vFpHYzNwJnRSK3UthZh8mXocIthjUNSW8wE87zrmmHPD2kYcHEibmY5tg373rzMWrPx4SpcixY_YjwLAS2rRce4zTFdPlnc4KrxxQa8DwpDV3o2L8vx0JhVz39O1tVvqzEI9y3bGJvHBk2F2-cHuNd4U21WZXwbD1F3U47q7YPuQNGy9cspEgVLWauhhoBrXG62F9_GdR_r4mtVH6rVrT4wzO4qZVVhWclW90ph25OMrdg8HsmGOd_wHUjYtojplEy_6wdyHweK072cxQe6Q2SCxvkBow9xJ-sX0sunsJKsZ41wx2ayCeb2g2WNBsbvKgnzHVzkTcz6DJmAEigtsaeTYZxkX_JnrHbVXPiPtwoBnQYKwU6RTuYLixYAcY17t6w5OJB-r4F2iBcVZKFu1el8MWE71flQYiGQkANNWBbTu-PaVcuLCt5Edd2Lb4CfIytu_xutRcjMTGZpiYvydBn7sFH9YjPUMsNZW-MMEvhfAb-DPM7MpNWDe7P049H3Cu0JgSjZmS2kadaVzrNb0VYJBjlLRM2oOcxy9302FfOc_7B0R7nNr-HuOuFLovgCE2-N9n096tucPPdHOPT2zA0u_59ZEt_O5dmMXHUv893mkWSNM9bL83aIRbVmLyuZw8hmImQexc290Cq3RIReSo52qX8ZCjikGpXTsRLlp0ftUT7MNQflIY3wZhn3B9WL0Gqf55ASS7F9f8ggF8vReEF59BhLgX8VNxk9q47SOIvUjBfqLrFZpEnwNY6Ek9vJ9MNBHLpglTp26gquGOaweCf89fSwUzV1X16BKyfGPGezR3Pi_2WmXj3bloMFg0kLZnKBUBLzuMGjha3OdKO_r2gwZSA2DHxeORkFFwYYzRYI5AffPsYp3NnLuqh1PU9WEN3qwYKXZTHgMMoClai1rxBcBa0TvqDN6a72pVH5bJ8SpAc7DNFE4A6Annat5Xl855M6zRIITOHWo2Y-2M5NPtSEtTTHxYTF_4qJbH4L51WF9dIHIi0wm3ivah595B7xAdcLYTcxnjvxiLLHhiw9Z4crTkuOr_qnsIpN8qGlTkCNw1kD-wXIfCp5riOZw6B3fvMOGwtbF7rYfK_59TXNdfEXicBOqU=w1920-h912',
+    linkpagina: 'www.google.com',
+    categoria_id: 1,
+  },
+  {
+    id: 2,
+    imagen: 'https://www.webfx.com/wp-content/uploads/2021/10/03-08_css3_cardtrick_ld_img.jpg',
+    linkpagina: 'www.google.com',
+    categoria_id: 1,
+  },
+  {
+    id: 3,
+    imagen: 'https://www.webfx.com/wp-content/uploads/2021/10/03-08_css3_cardtrick_ld_img.jpg',
+    linkpagina: 'none',
+    categoria_id: 1,
+  },
+  {
+    id: 4,
+    imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGVq9sHo9pPp4uySO0bdnmXbMDcxlUJ-1_TNRGPbwudg&s',
+    linkpagina: 'none',
+    categoria_id: 2,
+  },
+  {
+    id: 5,
+    imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGVq9sHo9pPp4uySO0bdnmXbMDcxlUJ-1_TNRGPbwudg&s',
+    linkpagina: 'none',
+    categoria_id: 2,
+  },
+  {
+    id: 6,
+    imagen: 'https://www.webfx.com/wp-content/uploads/2021/10/03-08_css3_cardtrick_ld_img.jpg',
+    linkpagina: 'none',
+    categoria_id: 2,
+  },
+  {
+    id: 7,
+    imagen: 'https://www.webfx.com/wp-content/uploads/2021/10/03-08_css3_cardtrick_ld_img.jpg',
+    linkpagina: 'none',
+    categoria_id: 2,
+  },
+  {
+    id: 8,
+    imagen: 'https://lospimas.com/cdn/shop/products/DSC_0631.recorte.jpg?v=1673484143',
+    linkpagina: 'none',
+    categoria_id: 2,
+  }
+]
+
+const tiendas = [
+  {
+    marca_id: 1,
+    titulo: 'Ropa de ',
+    productos: [
       {
-        modelo: 'Europe Street beat',
-        photo: 'https://i.postimg.cc/fbnB2yfj/na1.png',
-        description: 'OFERTA POLOS BASICOS HOMBRE',
-        price: 'S/. 29.00 PEN',
-        link: 'https://www.google.com/'
+        modelo: 'TOPITOP MUJER',
+        foto: 'https://lospimas.com/cdn/shop/products/DSC_0631.recorte.jpg?v=1673484143',
+        descripcion: 'Jean Mujer Micaela Celest',
+        precio: 's/ 200',
+        link: ''
       },
       {
-        modelo: 'Europe Street beat 1',
-        photo: 'https://lospimas.com/cdn/shop/products/DSC_0515completo_f81834e9-fc90-4607-a051-c18dcf93802b_540x.jpg?v=1673484493',
-        description: 'OFERTA POLOS BASICOS RAR',
-        price: 'S/. 129.00 PEN',
-        link: 'https://www.google.com/'
+        modelo: 'TOPITOP MUJER',
+        foto: 'https://lospimas.com/cdn/shop/products/DSC_0631.recorte.jpg?v=1673484143',
+        descripcion: 'Jean Mujer Micaela Celest',
+        precio: ' S/ 69.93',
+        link: ''
       },
       {
-        modelo: 'Europe Street beat 3',
-        photo: 'https://lospimas.com/cdn/shop/products/DSC_0653recortefondopuro_540x.jpg?v=1673484143',
-        description: 'OFERTA POLOS BASICOS HOMBRE',
-        price: 'S/. 29.00 PEN',
-        link: 'https://www.google.com/'
+        modelo: 'TOPITOP MUJER',
+        foto: 'https://lospimas.com/cdn/shop/products/DSC_0631.recorte.jpg?v=1673484143',
+        descripcion: 'Jean Mujer Micaela Celest',
+        precio: ' S/ 69.93',
+        link: ''
       },
       {
-        modelo: 'Europe Street beat',
-        photo: 'https://lospimas.com/cdn/shop/products/DSC_0906-2Camelcostado_540x.jpg?v=1673485229',
-        description: 'OFERTA POLOS BASICOS HOMBRE',
-        price: 'S/. 50.00 PEN',
-        link: 'https://www.google.com/'
+        modelo: 'TOPITOP MUJER',
+        foto: 'https://lospimas.com/cdn/shop/products/DSC_0631.recorte.jpg?v=1673484143',
+        descripcion: 'Jean Mujer Micaela Celest',
+        precio: ' S/ 69.93',
+        link: ''
       }
     ]
   }
 ];
 
-export default wow;
+
+
+export { 
+  categorias, marcas, tiendas
+}
--- src/pages/Landing/LandingPage.vue
+++ src/pages/Landing/LandingPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wow">
-    <nav :style="{ backgroundColor: navbarBackground, boxShadow: navbarShadow }">
+    <nav>
       <div class="container">
         <div class="nav-logos">
           <img src="../../assets/img/logos/logo-bicentenario.png" alt="cyberwowpnte">
@@ -10,39 +10,110 @@
         </div>
       </div>
     </nav>
-    
-    <main></main>
 
-    <section>
+    <main>
       <div class="container">
+        <div class="banner">
+          <div>
+            <a-carousel autoplay effect="fade">
+              <div class="wow-banner">
+                <img class="text" src="../../assets/img/wow/banner.png" alt="">
+                <img class="girl" src="../../assets/img/wow/banner-girl.png" alt="">
+              </div>
+              <div class="wow-banner">
+                <img class="text" src="../../assets/img/wow/banner-2.png" alt="">
+                <img class="girl2" src="../../assets/img/wow/banner-girl-2.png" alt="">
+              </div>
+            </a-carousel>
+          </div>
+        </div>
+      </div>
+    </main>
 
-        <div class="sales" v-for="(store,i) in wow" :key="i">
-          <h2>{{ store.brand }}</h2>
+    
+    
+    
+    <section>
+      <div class="container" v-if="active == 'category'">
+        <div class="categories">
+          <h2>Categorías</h2>
           <a-row :gutter="[16, 16]">
-            <a-col :xs="12" :sm="12" :md="12" :lg="6" :xl="6" v-for="(product, idx) in store.products" :key="idx">
-              <a-card hoverable @click="handleclick(product)">
-                <template #cover>
-                  <img alt="example" :src="product.photo" />
-                </template>
-                <a-card-meta :title="product.modelo">
-                  <template #description>
-                    <div>{{ product.description }}</div>
-                    <div style="font-size: 13px;"><span>A partir de</span> <b>{{ product.price }}</b></div>
-                  </template>
-                </a-card-meta>
-              </a-card>
+            <a-col :xs="12" :sm="12" :md="12" :lg="6" :xl="6" v-for="(category, idx) in categorias" :key="idx">
+              <div class="card" @click="handleFindBrands(category)">
+                <img style="width: 100%;" :src="category.imagen" alt="">
+              </div>
             </a-col>
           </a-row>
         </div>
-
-  
-
-     
       </div>
+
+
+      <div class="container" v-if="active == 'brand'">
+        <div class="brands">
+
+          <a-breadcrumb class="wow-links">
+            <a-breadcrumb-item><a @click="brands = null">Inicio</a></a-breadcrumb-item>
+            <a-breadcrumb-item>Categorías</a-breadcrumb-item>
+          </a-breadcrumb>
+
+          <a-row :gutter="[16, 16]">
+            <a-col :xs="12" :sm="12" :md="12" :lg="6" :xl="6" v-for="(brand, idx) in brands" :key="idx">
+              <div class="card" @click="handleclick(brand)">
+                <a-card hoverable>
+                  <template #cover>
+                    <img alt="example" :src="brand.imagen" />
+                  </template>
+                  <a-card-meta title="Nombre" description="description">
+                  </a-card-meta>
+                </a-card>
+              </div>
+            </a-col>
+          </a-row>
+        </div>
+      </div>
+
+
+
+      <div class="container" v-if="active == 'store'">
+        <div class="stores">
+          <a-breadcrumb class="wow-links">
+            <a-breadcrumb-item><a @click="brands = null">Inicio</a></a-breadcrumb-item>
+            <a-breadcrumb-item>Categorías</a-breadcrumb-item>
+          </a-breadcrumb>
+
+          <div class="sales" v-for="(store, i) in tiendas" :key="i">
+            <h2>{{ store.titulo }}</h2>
+            <a-row :gutter="[16, 16]">
+              <a-col :xs="12" :sm="12" :md="12" :lg="6" :xl="6" v-for="(brands, idx) in store.productos" :key="idx">
+                <a-card hoverable @click="handleclick(brands)">
+                  <template #cover>
+                    <img alt="example" :src="brands.foto" style="height: 270px;" />
+                  </template>
+                  <a-card-meta :title="brands.modelo">
+                    <template #description>
+                      <div>{{ brands.descripcion }}</div>
+                      <div><span>A partir de</span> <b>{{ brands.precio }}</b></div>
+                    </template>
+                  </a-card-meta>
+                </a-card>
+              </a-col>
+            </a-row>
+          </div>
+
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
     </section>
 
-    <br>
-    <br> 
 
     <FooterFormalization />
 
@@ -52,32 +123,33 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
 import FooterFormalization from '../Formalizacion/FormalizacionFooter.vue';
-import wow  from '@/json/wow.js'
+import { categorias, marcas, tiendas } from '@/json/wow.js'
 
-const navbarBackground = ref('transparent');
-const navbarShadow = ref('');
+const brands = ref(false);
+const stores = ref(false);
+const active = ref('category');
 
-const handleScroll = () => {
-  const scrollPosition = window.scrollY;
-  if (scrollPosition > 0) {
-    navbarBackground.value = '#fff';
-    navbarShadow.value = '0px 1px 10px #999';
+const handleFindBrands = (category) => {
+  active.value = 'brand';
+  const filteredBrands = marcas.filter(marca => marca.categoria_id === category.id);
+  if (filteredBrands.length > 0) {
+    brands.value = filteredBrands;
   } else {
-    navbarBackground.value = 'transparent';
-    navbarShadow.value = '';
+    console.log('No se encontraron marcas para la categoría especificada.');
   }
-};
+}
+
 
 const handleclick = (val) => {
-  window.open(val.link, '_blank');
+  active.value = 'store';
+  if(val.linkpagina == 'none') {
+    const filteredBrands = marcas.filter(marca => marca.marca_id === val.categoria_id);
+    stores.value = filteredBrands;
+  } else {
+    window.open(val.link, '_blank');
+  }
 }
-onMounted(() => {
-  window.addEventListener('scroll', handleScroll);
-});
 
-onUnmounted(() => {
-  window.removeEventListener('scroll', handleScroll);
-});
 </script>
 
 <style scoped>
@@ -90,14 +162,49 @@ onUnmounted(() => {
     height: 60px;
     transition: background-color 0.3s ease;
     z-index: 99;
+    background-color: #fff;
+    box-shadow: 0px 1px 10px #999;
   }
 
   main {
     padding-top: 60px;
-    height: 100vh;
+    background-size: 100% 500px;
     background-image: url('../../assets/img/wow/wow.png');
-    background-size: 100% 100%;
-    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    -o-background-size: cover;
+    -moz-background-size: cover;
+    -webkit-background-size: cover;
+
+    .banner {
+
+
+      .text {
+        width: 450px;
+      }
+
+      .girl {
+        width: 32%;
+        position: absolute;
+        right: 3rem;
+        bottom: 0;
+      }
+
+      .girl2 {
+        width: 630px;
+        position: absolute;
+        right: 6rem;
+        bottom: 0;
+      }
+
+      .wow-banner {
+        position: relative;
+        width: 100%;
+        display: flex !important;
+        height: 400px;
+        align-items: center;
+      }
+    }
   }
 
   .container {
@@ -109,23 +216,94 @@ onUnmounted(() => {
     }
   }
 
+  .categories,
+  .brands {
+    margin: 3rem 0;
+  }
+
   .nav-logos {
     display: flex;
     align-items: center;
     height: 100%;
     justify-content: space-between;
+
     img {
       height: 40px;
     }
   }
 
   .sales {
-    padding: 3rem 0 .5rem 0;
+    padding: 3rem 0;
+
     h2 {
       margin-bottom: 1rem;
     }
+
     b {
       color: #ff4d38;
+      font-size: 13px;
+    }
+  }
+
+  .categories {
+    .card {
+      img {
+        width: 100%;
+        /* height: 200px; */
+        border-radius: 6px;
+        cursor: pointer;
+      }
+
+      &:hover {
+        border-radius: 6px;
+        transition: box-shadow 0.2s, border-color 0.2s;
+        /* box-shadow: 0 1px 2px -2px rgba(0, 0, 0, 0.16), 0 3px 6px 0 rgba(0, 0, 0, 0.12), 0 5px 12px 4px rgba(0, 0, 0, 0.09); */
+      }
+    }
+  }
+
+  .wow-links {
+    background-color: transparent !important;
+    position: relative !important;
+    box-shadow: none !important;
+    z-index: 10;
+  }
+
+  @media screen and (max-width:992px) {
+    .sales {
+      b {
+        color: #ff4d38;
+        font-size: 12px;
+      }
+    }
+
+    .wow-banner {
+      justify-content: center;
+    }
+
+    .girl,
+    .girl2 {
+      display: none;
+    }
+  }
+}
+</style>
+
+<style lang="scss">
+.brands {
+  .ant-card-body {
+    padding: .8rem;
+  }
+
+  .ant-card-meta-title {
+    margin: 0 !important;
+  }
+
+  .ant-card-cover {
+    height: 170px;
+
+    img {
+      height: inherit;
     }
   }
 }
