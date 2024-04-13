42338244e76ca4b6ec53d6d8ed6ebcbd6fce6e52
JL <jloo6778@gmail.com>
2024-04-13 08:35:00 -0500

    wow

--- src/pages/Landing/LandingPage.vue
+++ src/pages/Landing/LandingPage.vue
@@ -248,7 +248,7 @@ const handleclick = (val) => {
         box-shadow: 0 1px 2px -2px rgba(0, 0, 0, 0.16), 0 3px 6px 0 rgba(0, 0, 0, 0.12), 0 5px 12px 4px rgba(0, 0, 0, 0.09);
       }
       h3 {
-        font-size: 22px;
+        font-size: 20px;
         font-family: cursive;
         font-family: "Roboto", sans-serif;
       }
