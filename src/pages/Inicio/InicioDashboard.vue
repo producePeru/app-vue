303c39d97ae77fff1948971389f75a23925b14e0
JL <jloo6778@gmail.com>
2024-05-07 08:19:44 -0500

    chart

--- src/pages/Inicio/InicioDashboard.vue
+++ src/pages/Inicio/InicioDashboard.vue
@@ -1,10 +1,80 @@
 <template>
+
+  <div>
+    <h3>Formalizaciones</h3>
+    <br>
+    <a-row>
+      <a-col :xs="12" :sm="12" :md="6" :lg="8" :xl="10">
+        <div id="chart" class="pie-pie">
+          <apexchart type="pie" height="290px" width="100%" :options="chartOptions" :series="series"></apexchart>
+        </div>
+      </a-col>
+    </a-row>
+  </div>
+
+
+
+
+
+
+
+
+
+
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { makeRequest } from '@/utils/api.js'
+
+const loading = ref(false);
+const series = ref([]);
+const chartOptions = {
+  chart: {
+    width: 450,
+    type: 'pie',
+    toolbar: {
+      show: true,
+      tools: {
+        download: true
+      },
+    }
+  },
+  labels: ['Asesorías', 'Formalizaciones RUC 10', 'Formalizaciones RUC 20'],
+
+};
+
+const fetchData = async () => {
+  loading.value = true;
+
+  try {
+    const data = await makeRequest({ url: `formalization/chart-pie`, method: 'GET' });
+    series.value = data
+    
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loading.value = false;
+  }
+};
+
+onMounted(() => {
+  fetchData();
+});
+</script>
+
+
+
+
+
+
+<!-- <template>
   <div>
 
-    <!-- <iframe
+    <iframe
       src="https://app.powerbi.com/view?r=eyJrIjoiNTBkNDY1OGUtMTdlZi00MjExLTg5YmEtODRiYThjOTAzZjc3IiwidCI6Ijc1MDRlMzE4LThlMWUtNGQ1NS1iZmZkLTg3NWI0ZGVlODI2MCIsImMiOjR9"
       frameborder="0" allowFullScreen="true">
-    </iframe> -->
+    </iframe>
 
   </div>
 </template>
@@ -16,4 +86,4 @@ iframe {
   height: 83vh;
   width: 100%;
 }
-</style>
+</style> -->
