7f15ca9d1a9daee3488e730781130e8ffb4194c0
HP PRODUCE <produce@gmail.com>
2024-07-25 18:40:30 -0500

    inicio

--- src/pages/Inicio/InicioDashboard.vue
+++ src/pages/Inicio/InicioDashboard.vue
@@ -1,14 +1,20 @@
 <template>
   <section>
-    <Formalizaciones v-if="storageRole[0].id == 1 || storageRole[0].id == 2" />
+    <Formalizaciones v-if="includesRole([1, 2])" />
   </section>
 </template>
 
 <script setup>
 import Formalizaciones from './InicioFormalizaciones.vue';
 
-const storageRole = JSON.parse(localStorage.getItem('role'));
+const storageRole = JSON.parse(localStorage.getItem('role')) || [];
+
+// Función para verificar si uno de los roles específicos está en el array de roles
+const includesRole = (roleIds) => {
+  return storageRole.some(role => roleIds.includes(role.id));
+};
 </script>
+
 <!-- import eventBus from '@/utils/bus.js'; -->
 <!-- eventBus.config.globalProperties.eventBus.$emit('parentClick', '¡Hola desde el padre!');
  -->
