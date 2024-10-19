59b3c5a1adf124f96a5d6932e4f3d0036fc6ed09
HP PRODUCE <produce@gmail.com>
2024-10-18 19:23:58 -0500

    ferias

--- src/pages/Ferias/PaginaRegistrarmeFeria.vue
+++ src/pages/Ferias/PaginaRegistrarmeFeria.vue
@@ -10,76 +10,81 @@
       </div>
     </header>
 
+    <section v-if="caducated">
 
-    <section class="fair">
-      <div class="container containerx" style="margin-top: 1rem; border-radius: 8px 8px 0 0;">
-
-        <div style="padding-top: 3rem;">
-          <div class="fair-title">
-            <h1>POSTULACIONES PERÚ PRODUCE BARRANCA</h1>
-            <h4>PLAZA DE ARMAS DE BARRANCA</h4>
-          </div>
-
-          <div class="fair-header">
-            <img class="fair-header-img"
-              src="https://www.mediasource.mx/hs-fs/hubfs/tipos-de-empresas-definicion-y-clasificacion.png?width=950&name=tipos-de-empresas-definicion-y-clasificacion.png"
-              alt="">
-            <div class="tag-html">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque omnis assumenda, aut repudiandae
-                consequatur blanditiis! Blanditiis ratione aliquid consequuntur, vel sequi natus. Inventore itaque totam
-                autem nulla nobis nihil enim.
-              </p>
-            </div>
-          </div>
-        </div>
-
-      </div>
-
-      <div class="fair-section">
-        <div class="container" style="background-color: #def1f8;">
-          <p>
-            <b>Importante:</b> La información proporcionada tiene carácter de Declaración Jurada, la misma que se
-            formula
-            en el marco de lo establecido en el artículo 49 del TUO de la Ley 27444 Ley del Procedimiento Administrativo
-            General, manifestando someterme a la normatividad vigente respecto a la materia y a las responsabilidades
-            administrativas, civiles y/o penales que se pudieran derivar en caso que alguno DE LOS DATOS CONSIGNADOS
-            SEAN
-            FALSOS, INEXACTOS, siendo pasible de cualquier fiscalización posterior que el Ministerio de la Producción
-            considere pertinente.
-          </p>
-        </div>
-      </div>
     </section>
 
 
-    <section class="form-x fair" >
-      <div class="container containerx" style="margin-bottom: 2rem; border-radius: 0 0 8px 8px;">
-        <div class="form-x-wrapper" style="padding: 2rem 1.5rem;">
 
-          <!-- formulario -->
-          <section>
+    <section v-else>
+      <section class="fair">
+        <div class="container containerx" style="margin-top: 1rem; border-radius: 8px 8px 0 0;">
 
-            <a-steps
-            @change="handleSteepState"
-            v-model:current="current"
-            status="error"
-            :items="[
-              {title: 'Empresa'},
-              {title: 'Representante Legal'},
-              {title: 'Participar'}
-            ]" />
-
-            <div style="margin-top: 2rem;">
-              <RegistroPaso1 v-if="current == 0" />
-              <RegistroPaso2 v-if="current == 1" />
-              <RegistroPaso3 v-if="current == 2" />
+          <div style="padding-top: 3rem;">
+            <div class="fair-title">
+              <h1>POSTULACIONES PERÚ PRODUCE BARRANCA</h1>
+              <h4>PLAZA DE ARMAS DE BARRANCA</h4>
             </div>
 
-          </section>
+            <div class="fair-header">
+              <img class="fair-header-img"
+                src="https://www.mediasource.mx/hs-fs/hubfs/tipos-de-empresas-definicion-y-clasificacion.png?width=950&name=tipos-de-empresas-definicion-y-clasificacion.png"
+                alt="">
+              <div class="tag-html">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque omnis assumenda, aut repudiandae
+                  consequatur blanditiis! Blanditiis ratione aliquid consequuntur, vel sequi natus. Inventore itaque
+                  totam
+                  autem nulla nobis nihil enim.
+                </p>
+              </div>
+            </div>
+          </div>
 
         </div>
-      </div>
+
+        <div class="fair-section">
+          <div class="container" style="background-color: #def1f8;">
+            <p>
+              <b>Importante:</b> La información proporcionada tiene carácter de Declaración Jurada, la misma que se
+              formula
+              en el marco de lo establecido en el artículo 49 del TUO de la Ley 27444 Ley del Procedimiento
+              Administrativo
+              General, manifestando someterme a la normatividad vigente respecto a la materia y a las responsabilidades
+              administrativas, civiles y/o penales que se pudieran derivar en caso que alguno DE LOS DATOS CONSIGNADOS
+              SEAN
+              FALSOS, INEXACTOS, siendo pasible de cualquier fiscalización posterior que el Ministerio de la Producción
+              considere pertinente.
+            </p>
+          </div>
+        </div>
+      </section>
+
+
+      <section class="form-x fair">
+        <div class="container containerx" style="margin-bottom: 2rem; border-radius: 0 0 8px 8px;">
+          <div class="form-x-wrapper" style="padding: 2rem 1.5rem;">
+
+            <!-- formulario -->
+            <section>
+
+              <a-steps @change="handleSteepState" v-model:current="current" status="error" :items="[
+                { title: 'Empresa' },
+                { title: 'Representante Legal' },
+                { title: 'Participar' }
+              ]" />
+
+              <div style="margin-top: 2rem;">
+                <RegistroPaso1 v-if="current == 0" />
+                <RegistroPaso2 v-if="current == 1" />
+                <RegistroPaso3 v-if="current == 2" />
+              </div>
+
+            </section>
+
+          </div>
+        </div>
+      </section>
     </section>
 
     <FooterFormalization />
@@ -94,6 +99,7 @@ import RegistroPaso1 from './components/RegistroPaso1.vue';
 import RegistroPaso2 from './components/RegistroPaso2.vue';
 import RegistroPaso3 from './components/RegistroPaso3.vue';
 
+const caducated = ref(true);
 const current = ref(0);
 const navBar = ref(null);
 const isFloating = ref(false);
@@ -108,7 +114,7 @@ const handleScroll = () => {
 
 const handleSteepState = () => {
   console.log("Steep state");
-  
+
 }
 
 onMounted(() => {
@@ -150,9 +156,10 @@ header {
 .fair-wrapper {
   background-color: #F7F7FA;
 }
+
 .fair {
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
-  
+
   .containerx {
     background-color: #fff;
     // box-shadow: 0px 1px 10px #cbcbcb;
